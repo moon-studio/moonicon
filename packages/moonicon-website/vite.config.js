fb9d42bd7bd375d16d619e742ad2752b15c0ee73
MiyueSC <913784771@qq.com>
2023-02-16 10:56:06 +0800

    Fix: Relative base url

--- packages/moonicon-website/vite.config.js
+++ packages/moonicon-website/vite.config.js
@@ -7,6 +7,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? './' : '/',
   plugins: [
     vue(),
     vueJsx(),
