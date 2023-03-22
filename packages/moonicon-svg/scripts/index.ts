526a1a32af3b1c3fc0d2c2fa5ecd22fdbce221b9
MiyueSC <913784771@qq.com>
2023-03-22 11:21:30 +0800

    fix: Change the spaces and - all to capital

--- packages/moonicon-svg/scripts/index.ts
+++ packages/moonicon-svg/scripts/index.ts
@@ -16,6 +16,14 @@ const svgComponentsFilePath = path.resolve(
     : path.join(_dirname, '../../moonicon-vue3/components/')
 )
 
+const toUpperCaseCamelCase = (str: string): string => {
+  return str
+    .replace(/[-\s]+/g, ' ') // 把连续的空格或短横线替换成一个空格
+    .split(' ') // 以空格为分隔符将字符串分割成单词数组
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1)) // 将每个单词的首字母大写
+    .join('') // 将单词数组合并成一个字符串
+}
+
 const v3ComponentTemplate = (filename: string, shapeStr: string) => {
   return `
     import { defineComponent, h } from 'vue'
@@ -60,6 +68,7 @@ const v3ComponentTemplate = (filename: string, shapeStr: string) => {
 }
 
 const checkOutputDir = (filename: string, template: string) => {
+  filename = toUpperCaseCamelCase(filename)
   if (fs.existsSync(svgComponentsFilePath)) {
     writeV3Components(filename, template)
   } else {
