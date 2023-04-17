f0934aa7e97b5de177cb4f81a6f00cf53a559536
songjianet <1778651752@qq.com>
2023-04-17 21:58:01 +0800

    [Feat][Website] Internationalization is supported.

--- packages/moonicon-website/src/views/icon/components/download/index.tsx
+++ packages/moonicon-website/src/views/icon/components/download/index.tsx
@@ -1,14 +1,16 @@
 import styles from './index.module.scss'
+import { useI18n } from 'vue-i18n'
 import { NButton } from 'naive-ui'
 
 export default defineComponent({
   name: 'Download',
   setup() {},
   render() {
+    const { t } = useI18n()
     return (
       <div class={styles.iconDownload}>
         <NButton class='m-block' type={'primary'} size='large'>
-          Download SVG
+          {t('icon.download-svg')}
         </NButton>
       </div>
     )
